e4b7ec1387aa0b8490f000a702621d3f18710c5f
Meysam Bavi <meybsky@gmail.com>
2021-05-18 19:47:31 +0430

    Improved cart item and fund dialog

--- lib/cart_item_card.dart
+++ lib/cart_item_card.dart
@@ -9,96 +9,108 @@ class CartItem extends StatefulWidget {
 }
 
 class _CartItemState extends State<CartItem> {
-  var _formKey = GlobalKey<FormState>();
+
   TextStyle headerStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: CommonColors.black);
   TextStyle otherStyle = TextStyle(fontSize: 15, color: CommonColors.black);
-  
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child : ExpansionTile(
           title: Text(widget.order.restaurant.name , style:headerStyle,),
-          subtitle:Text(Strings.formatDate(widget.order.time)),
-          trailing:Text(widget.order.totalCost.toString()),
+          subtitle: Wrap(
+            direction: Axis.vertical,
+            spacing: 5,
+            children: [
+              Text(Strings.formatDate(widget.order.time), style: Theme.of(context).textTheme.caption),
+              Text(widget.order.customer.address.text, style: Theme.of(context).textTheme.caption),
+            ],
+          ),
+          trailing: Text(widget.order.totalCost.toString()),
           expandedCrossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            ...buildListOfWidget(),
+            ...buildListOfItems(),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                buildModelButton(Strings.get('cart-page-proceed')!, CommonColors.green!, (){
-                  var userCredit = (Head.of(context).server.account as UserAccount).credit.toInt();
-                  if (userCredit >= widget.order.totalCost.toInt())
-                  {
-                    // cant add to owner account !!!!
-                    Head.of(context).server.addNewOrder(widget.order);
-                    (Head.of(context).server.account as UserAccount).activeOrders.add(widget.order);
-                    (Head.of(context).server.account as UserAccount).cart.remove(widget.order);
-                    (Head.of(context).server.account as UserAccount).credit-= widget.order.totalCost;
-                    ScaffoldMessenger.of(context).showSnackBar(showBar(Strings.get('order-completed')!, Duration(milliseconds: 2000)));
-                    widget.rebuildMenu();
-                  } else
-                    {
-                      showDialog(context: context, builder: (context)=>buildInsufficientFundDialog());
-                    }
-                  }
-                ),
-                buildModelButton(Strings.get('cart-page-delete')!, CommonColors.red!, () async {
-                  var res = await showDialog(
-                      context: context,
-                      builder: (context)=>buildRemoveDialog()
-                  );
-                  if (res) {
-                    (Head.of(context).server.account as UserAccount).cart.remove(widget.order);
-                    widget.rebuildMenu();
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      showBar(Strings.get('delete_order')!, Duration(milliseconds: 2000))
-                    );
-                  } else return;
-                })
+                buildModelButton(Strings.get('cart-page-proceed')!, CommonColors.green!, proceedPressed),
+                buildModelButton(Strings.get('cart-page-delete')!, CommonColors.red!, deletePressed)
               ],
             )
           ],
       )
     );
   }
-  Widget buildOrderItem(FoodData o , int i)
-  {
-    return Padding(
-        padding: EdgeInsets.all(10),
-        child: Card(
-          child: ListTile(
-            title: Text(o.name , style:headerStyle,),
-            subtitle: Text(o.price.toString() + ' X ' + i.toString() + ' = '+(o.price*Price(i)).toString() , style: otherStyle,),
-            trailing: IconButton(
-              icon: Icon(Icons.clear,color: CommonColors.red,),
-              onPressed: () async {
-                var res = await showDialog(
-                    context: context,
-                    builder: (context)=>buildRemoveDialog()
-                );
-                if (!res) return;
-                  setState(() {
-                    widget.order.items.remove(o);
-                  if (widget.order.items.isEmpty) {
-                    (Head.of(context).server.account as UserAccount).cart.remove(widget.order);
-                    widget.rebuildMenu();
-                  }
-                  });
-              },
-            ),
-          ),
-        ),
-    );
-  }
-  List<Widget> buildListOfWidget()
-  {
+
+  List<Widget> buildListOfItems() {
     List<Widget> retValue = [];
     widget.order.items.forEach((key, value) {retValue.add(buildOrderItem(key, value));});
     print('$retValue');
     return retValue;
   }
+
+  Widget buildOrderItem(FoodData data , int count) {
+    return Padding(
+      padding: EdgeInsets.all(10),
+      child: Card(
+        child: ListTile(
+          title: Text(data.name , style: headerStyle,),
+          subtitle: Text('${data.price} × $count = ${data.price.toInt() * count}' , style: otherStyle,),
+          trailing: IconButton(
+            icon: Icon(Icons.remove_circle_rounded, color: CommonColors.red,),
+            onPressed: () async {
+              var res = await showDialog(
+                  context: context,
+                  builder: (context)=>buildRemoveDialog()
+              );
+              if (!res) return;
+              setState(() {
+                widget.order.items.remove(data);
+                if (widget.order.items.isEmpty) {
+                  (Head.of(context).server.account as UserAccount).cart.remove(widget.order);
+                  widget.rebuildMenu();
+                }
+              });
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  void proceedPressed(){
+
+    var user = Head.of(context).server.account as UserAccount;
+
+    if (widget.order.totalCost.toInt() > user.credit.toInt()) {
+      showDialog(context: context, builder: (context) => buildInsufficientFundDialog());
+      return;
+    }
+    widget.order.sendRequest();
+    user.activeOrders.add(widget.order);
+    user.cart.remove(widget.order);
+    user.credit -= widget.order.totalCost;
+    widget.rebuildMenu();
+    ScaffoldMessenger.of(context).showSnackBar(
+      showBar(Strings.get('order-completed')!, Duration(milliseconds: 2000),),
+    );
+  }
+
+  void deletePressed() async {
+    var result = await showDialog(
+        context: context,
+        builder: (context) => buildRemoveDialog()
+    );
+
+    if (result == null || result == false) return;
+
+    (Head.of(context).server.account as UserAccount).cart.remove(widget.order);
+    widget.rebuildMenu();
+    ScaffoldMessenger.of(context).showSnackBar(
+        showBar(Strings.get('delete_order')!, Duration(milliseconds: 2000))
+    );
+  }
+
   buildRemoveDialog() {
     return AlertDialog(
       title: Text(Strings.get('food-remove-dialog-title')!),
@@ -119,45 +131,52 @@ class _CartItemState extends State<CartItem> {
       ],
     );
   }
-  buildInsufficientFundDialog()
-  {
+
+  buildInsufficientFundDialog() {
+
     var user = (Head.of(context).server.account as UserAccount);
+    var _formKey = GlobalKey<FormState>();
+
     return AlertDialog(
       title: Text(Strings.get('insufficient-fund-dialog')!),
+      content: Text('${Strings.get('current-credit-message')} ${user.credit} ${Strings.get('toman')}.'),
       titleTextStyle: headerStyle,
       actions: [
-       Form
-        (
-           key:_formKey,
-           child :Container(
-             width: 300,
-             height: 50,
-             child:TextFormField(
-             keyboardType: TextInputType.number,
-             validator: (value) {
-               if (value == null || value.isEmpty)
-                 return Strings.get('add-fund-null-error');
-               var intValue = int.tryParse(value);
-               if (intValue! <= 0)
-                 return Strings.get('add-fund-negative-error');
-               },
-             onSaved: (value) => user.credit += Price(int.tryParse(value!)!),
-             decoration: InputDecoration(
-               hintText: Strings.get('add-fund-hint'),
-             ),
-           ),
-           )
+        Form(
+          key: _formKey,
+          child : Container(
+            width: 300,
+            height: 50,
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return Strings.get('add-fund-null-error');
+                }
+                var intValue = int.tryParse(value);
+                if (intValue == null) {
+                  return Strings.get('add-fund-invalid-number');
+                }
+                if (intValue <= 0) {
+                  return Strings.get('add-fund-negative-error');
+                }
+              },
+              onSaved: (value) => user.credit += Price(int.tryParse(value!)!),
+              decoration: InputDecoration(
+                hintText: Strings.get('add-fund-hint'),
+              ),
+            ),
+          ),
         ),
         TextButton(
             onPressed: (){
-              if (_formKey.currentState!.validate())
-                _formKey.currentState!.save();
-              _formKey.currentState!.reset();
+              if (!_formKey.currentState!.validate()) return;
+              _formKey.currentState!.save();
               Navigator.of(context).pop();
             },
-            child: Text(Strings.get('fund-dialog-add-fund')!,style: otherStyle,)
-        )
+            child: Text(Strings.get('fund-dialog-add-fund')!, style: otherStyle,)
+        ),
       ],
-    );  
+    );
   }
 }
