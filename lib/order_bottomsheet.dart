3f0ef135ef7d0de018e5a07bb5171e79d071d9a8
Meysam Bavi <meybsky@gmail.com>
2021-05-20 02:08:52 +0430

    Cleaned order bottom sheet and made it stateless

--- lib/order_bottomsheet.dart
+++ lib/order_bottomsheet.dart
@@ -1,92 +1,98 @@
 import 'package:flutter/material.dart';
 import 'package:models/models.dart';
 
-class OrderFood extends StatefulWidget {
-  final Food food;
+class OrderFood extends StatelessWidget {
+  static int _numberOfFoods = 0;
+  static final _formKey = GlobalKey<FormState>();
 
+  final Food food;
   OrderFood(this.food) : super();
 
   @override
-  _OrderFoodState createState() => _OrderFoodState();
-}
-
-class _OrderFoodState extends State<OrderFood> {
-  TextStyle headerStyle = TextStyle(
-      fontSize: 20, fontWeight: FontWeight.bold, color: CommonColors.black);
-  TextStyle otherStyle = TextStyle(fontSize: 15, color: CommonColors.black);
-  var _formKey = GlobalKey<FormState>();
-  int numberOfFoods = 0;
-
-  @override
   Widget build(BuildContext context) {
-    Size phoneSize = MediaQuery.of(context).size;
-    return Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-            child: Container(
-              child: Column(
-                children: [
-                  Container(
-                    width: phoneSize.width,
-                    height: phoneSize.height / 4,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                  Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Row(
-                        children: [
-                          Text(
-                            widget.food.name,
-                            style: headerStyle,
-                          ),
-                          Spacer(),
-                          Text(
-                            widget.food.price.toString(),
-                            style: otherStyle,
-                          )
-                        ],
-                      )
-                  ),
-                  Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        widget.food.description,
-                        style: otherStyle,
-                      )
-                    ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: Strings.get('order-bottom-sheet-number'),
-                      ),
-                      keyboardType: TextInputType.number,
-                      validator: (String? num) {
-                        if (num == null || num.isEmpty)
-                          return Strings.get('order-bottom-sheet-empty-error');
-                        var parsed = int.tryParse(num);
-                        if (parsed == null || parsed <= 0)
-                          return Strings.get('order-bottom-sheet-negative-error');
-                        if (!widget.food.isAvailable)
-                          return Strings.get('order-bottom-sheet-not-available');
-                      },
-                      onSaved: (num) => numberOfFoods = int.tryParse(num!)!,
-                    )
-                  ),
-                buildModelButton(
-                    Strings.get('order-bottom-sheet-order')!, CommonColors.green!,
-                    () {
-                      if (_formKey.currentState!.validate()) {
-                        _formKey.currentState!.save();
 
-                        Navigator.pop(context, {widget.food.toFoodData(): numberOfFoods});
-                  }
-                }
-              )
-            ],
-          ),
-        )));
+    Size phoneSize = MediaQuery.of(context).size;
+    TextStyle headerStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: CommonColors.black);
+    TextStyle otherStyle = TextStyle(fontSize: 15, color: CommonColors.black);
+
+    return SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: phoneSize.width,
+              height: phoneSize.height / 4,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(food.name, style: headerStyle,),
+                  Text(food.price.toString(), style: otherStyle,)
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text('Description: ', style: otherStyle,),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(food.description, style: otherStyle,),
+            ),
+            if (food.isAvailable)
+              Padding(
+                  padding: EdgeInsets.all(10),
+                  child: buildNumberField()
+              ),
+            if (food.isAvailable)
+              Center(
+                child: buildModelButton(Strings.get('order-bottom-sheet-order')!, CommonColors.green!, () => orderPressed(context)),
+              ),
+          ],
+        )
+    );
+  }
+
+  Widget buildNumberField() {
+    return Form(
+      key: _formKey,
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: Strings.get('order-bottom-sheet-number'),
+        ),
+        keyboardType: TextInputType.number,
+        validator: (String? num) {
+          if (num == null || num.isEmpty) {
+            return Strings.get('order-bottom-sheet-empty-error');
+          }
+
+          var parsed = int.tryParse(num);
+
+          if (parsed == null) {
+            return Strings.get('add-fund-invalid-number');
+          }
+
+          if (parsed <= 0) {
+            return Strings.get('order-bottom-sheet-negative-error');
+          }
+
+          if (!food.isAvailable) {
+            return Strings.get('order-bottom-sheet-not-available');
+          }
+        },
+        onSaved: (num) => _numberOfFoods = int.tryParse(num!)!,
+      ),
+    );
+  }
+  orderPressed(BuildContext context) {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      Navigator.pop(context, {food.toFoodData(): _numberOfFoods});
+    }
   }
 }
