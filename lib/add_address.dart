267cc856be2360035e411eb06f28a9c578c44a5a
Meysam Bavi <meybsky@gmail.com>
2021-05-21 00:19:37 +0430

    Create add address page with map (UNCOMPLETE)

---
+++ lib/add_address.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddAddressPage extends StatefulWidget {
+
+  @override
+  _AddAddressPageState createState() => _AddAddressPageState();
+}
+
+class _AddAddressPageState extends State<AddAddressPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
